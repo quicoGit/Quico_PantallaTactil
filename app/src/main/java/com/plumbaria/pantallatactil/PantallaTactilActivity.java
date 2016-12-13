c968ebc5a08fef4693d99b92655aa07863fbf447
Quico Orozco <esi@plumbaria.com>
2016-12-13 16:58:23 +0100

    Initial commit

---
+++ app/src/main/java/com/plumbaria/pantallatactil/PantallaTactilActivity.java
@@ -0,0 +1,13 @@
+package com.plumbaria.pantallatactil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PantallaTactilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
