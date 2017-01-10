a42ec038def13a33d368f39fa89ac9f156c9e1c2
Quico Orozco <esi@plumbaria.com>
2017-01-10 18:16:34 +0100

    Ejercicio 5_9

--- app/src/main/java/com/plumbaria/pantallatactil/PantallaTactilActivity.java
+++ app/src/main/java/com/plumbaria/pantallatactil/PantallaTactilActivity.java
@@ -19,7 +19,22 @@ public class PantallaTactilActivity extends AppCompatActivity implements View.On
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
         TextView salida = (TextView) findViewById(R.id.TextViewSalida);
-        salida.append("\n" + motionEvent.toString()+"\n" );
+        //salida.append("\nPresión: " + presion + "\n" + "Tamaño: " + tam + "\n");
+        //salida.append("\n" + motionEvent.toString()+"\n" );
+        String acciones[] = { "ACTION_DOWN", "ACTION_UP", "ACTION_MOVE",
+                "ACTION_CANCEL", "ACTION_OUTSIDE", "ACTION_POINTER_DOWN",
+                "ACTION_POINTER_UP" };
+        int accion = motionEvent.getAction();
+        int codigoAccion = accion & MotionEvent.ACTION_MASK;
+        int iPuntero = (accion & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+        salida.append("\n" + acciones[codigoAccion]);
+        salida.append(" Generada por puntero: " + iPuntero);
+        for (int i = 0; i < motionEvent.getPointerCount(); i++) {
+            salida.append("\npuntero:" + motionEvent.getPointerId(i) +
+                    " x:" + motionEvent.getX(i) + " y:" + motionEvent.getY(i));
+        }
+        salida.append("\n");
         return true;
     }
 }
