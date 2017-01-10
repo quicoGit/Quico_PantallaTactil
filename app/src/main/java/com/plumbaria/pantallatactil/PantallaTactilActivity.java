27ad0d4f3ffa099ef739ed09a8d4192f37294f6a
Quico Orozco <esi@plumbaria.com>
2017-01-10 17:56:52 +0100

    Final ejercicio 5_8

--- app/src/main/java/com/plumbaria/pantallatactil/PantallaTactilActivity.java
+++ app/src/main/java/com/plumbaria/pantallatactil/PantallaTactilActivity.java
@@ -2,12 +2,24 @@ package com.plumbaria.pantallatactil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
-public class PantallaTactilActivity extends AppCompatActivity {
+public class PantallaTactilActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView entrada = (TextView)findViewById(R.id.TextViewEntrada);
+        entrada.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        TextView salida = (TextView) findViewById(R.id.TextViewSalida);
+        salida.append("\n" + motionEvent.toString()+"\n" );
+        return true;
     }
 }
